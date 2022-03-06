bd22302103e102fb9509bd8249aca485d7ec959b
Hoang Son <soonhoang14@gmail.com>
2022-03-06 14:07:40 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    "BASE_URL": "next-ecommerce-kappa-two.vercel.app",
+    "BASE_URL": "https://next-ecommerce-kappa-two.vercel.app/",
     "MONGO_URL": "mongodb+srv://hson:hson@cluster0.vamke.mongodb.net/next_app?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET": "'!wPzP[9'9Cv')C4rPJu+]&+/<%QMV~JP$9cx<e:b.uq4s!V,u",
     "REFRESH_TOKEN_SECRET": "L/ZX[J*Cxs@z8rztCX.qW\yNTyaZ<zEN_$3aAA\\9r4;*Mq-#PbncP`8,39BBy#?w.w)}\"2R`C-GpdX'Y",
